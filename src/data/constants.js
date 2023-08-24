d4d5af212232404a46f4e42098776628827f7e09
iHoerth <hoerth.ignacio@gmail.com>
2023-08-24 11:52:23 -0300

    Project setup: constants and thumbnails

---
+++ src/data/constants.js
@@ -0,0 +1,178 @@
+export const Bio = {
+  name: 'Ignacio Hoerth',
+  roles: [
+    'Fullstack Developer',
+    'Frontend Developer',
+    'Backend Developer',
+    'Junior React Developer',
+    'UI/UX Designer',
+  ],
+  description: `
+  I'm a JavaScript developer specializing in React, known for my strong problem-solving skills and passion for teaching. I thrive on understanding how things work and am always eager to take on new challenges.`,
+  github: 'https://github.com/iHoerth',
+  resume_eng: `https://drive.google.com/file/d/1CY37wOnzhhYWbQ_OQyWMsGfAq2RbQ1Y_/view?usp=drive_link`,
+  resume_esp: `https://drive.google.com/file/d/1e0QvstVcMW43p7YfrLEbzKqMwKHwvJju/view?usp=drive_link`,
+  linkedin: 'https://www.linkedin.com/in/ignaciohoerth/',
+  twitter: 'https://twitter.com/nachohoerth',
+  insta: 'https://www.instagram.com/nachohoerth/',
+};
+
+export const skills = [
+  {
+    title: 'Frontend',
+    skills: [
+      {
+        name: 'React Js',
+        image:
+          'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
+      },
+      {
+        name: 'Redux',
+        image:
+          'https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg',
+      },
+      {
+        name: 'Next Js',
+        image:
+          'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=',
+      },
+      {
+        name: 'HTML',
+        image: 'https://www.w3.org/html/logo/badge/html5-badge-h-solo.png',
+      },
+      {
+        name: 'CSS',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png',
+      },
+      {
+        name: 'JavaScript',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png',
+      },
+      {
+        name: 'Bootstrap',
+        image: 'https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png',
+      },
+      {
+        name: 'Material UI',
+        image: 'https://static-00.iconduck.com/assets.00/material-ui-icon-512x406-fhnu85xg.png',
+      },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      {
+        name: 'Node Js',
+        image: 'https://nodejs.org/static/images/logo.svg',
+      },
+      {
+        name: 'Express Js',
+        image: '',
+      },
+      {
+        name: 'Postgresql',
+        image: 'https://www.postgresql.org/media/img/about/press/elephant.png',
+      },
+      {
+        name: 'MongoDB',
+        image:
+          'https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg',
+      },
+      {
+        name: 'Firebase',
+        image: 'https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg',
+      },
+    ],
+  },
+  {
+    title: 'Others',
+    skills: [
+      {
+        name: 'Git',
+        image:
+          'https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667',
+      },
+      {
+        name: 'GitHub',
+        image: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+      },
+      {
+        name: 'Netlify',
+        image: 'https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png',
+      },
+      {
+        name: 'VS Code',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519',
+      },
+      {
+        name: 'Postman',
+        image:
+          'https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667',
+      },
+      {
+        name: 'Figma',
+        image:
+          'https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667',
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: '',
+    role: '',
+    company: '',
+    date: '',
+    desc: '',
+    skills: [],
+    doc: '',
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: '',
+    school: '',
+    date: '',
+    grade: '',
+    desc: '',
+    degree: '',
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: 'PokeFinder',
+    date: 'May 2023',
+    description: `Web app que simula ser un PokeDex para buscar cualquier pokemon, sus estadisticas, tipos. Proximamente se añadiran features para armar una party de 6 pokemones y calcular debilidades. `,
+    image: 'pokefinder_thumbnail.png',
+    tags: ['React Js', 'React Context', 'Material UI'],
+    category: 'web app',
+    github: 'https://github.com/iHoerth/PokeFinder',
+    webapp: 'https://pokefinder-iota.vercel.app/',
+  },
+  {
+    id: 1,
+    title: 'Consumedic',
+    date: 'June 2023',
+    description: `Web app que permite a los pacientes buscar medicos por especialidad, agendar y pagar turnos, y llevar un historial clinico online.`,
+    image: 'consumedic_thumbnail.png',
+    tags: ['React Js', 'Postgresql', 'Node Js', 'Express Js', 'React Context', 'Material UI'],
+    category: 'web app',
+    github: 'https://github.com/iHoerth/consumedic',
+    webapp: 'https://pf-soyhenry.vercel.app/',
+  },
+];
+
+export const TimeLineData = [
+  { year: 2021, text: 'Started studying C and JavaScript' },
+  { year: 2022, text: 'Graduated at Coderhouse courses and Developed my first React projects' },
+  { year: 2023, text: 'Graduated at SoyHenry bootcamp and started my journey' },
+];
