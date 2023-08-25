db0d26e52e57f3399635b82c81ab64f25b1dfb37
iHoerth <hoerth.ignacio@gmail.com>
2023-08-25 13:06:45 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Skills from './components/Skills/Skills';
+import Education from './components/Education/Education';
 
 import { darkTheme } from './utils/themes';
 import './App.css';
@@ -16,8 +17,8 @@ const Body = styled.div`
 `;
 
 const Wrapper = styled.div`
-  background: linear-gradient(38.73 deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%),
-    linear-gradient(141.27 deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  background: linear-gradient(40deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%),
+    linear-gradient(140deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
   width: 100%;
   clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
 `;
@@ -29,8 +30,10 @@ function App() {
         <Body>
           <NavBar />
           <Header />
+
           <Wrapper>
             <Skills />
+            <Education />
           </Wrapper>
         </Body>
       </BrowserRouter>
